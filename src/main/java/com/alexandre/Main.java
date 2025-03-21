74d92069d08a3f8a7d7a77dc13f4558a1e3a899d
Alexandre <alex12.fernando12@gmail.com>
2025-03-21 18:00:27 -0300

    alexandre

--- src/main/java/com/alexandre/Main.java
+++ src/main/java/com/alexandre/Main.java
@@ -1,5 +1,23 @@
 package com.alexandre;
 
+import java.net.Socket;
+
+import com.alexandre.server.ChatServer;
+
 public class Main {
-    public static void main(String[] args) {App.main(args);}
+    public static void main(String[] args) {
+        
+
+        App.main(args);
+    }
+
+    public static void acceptClients() {
+        while(true) {
+            try {
+                Socket socket = ChatServer.serverSocket.accept();
+            } catch (Exception e) {
+                System.out.println("Erro ao aceitar clientes 001: " + e.getMessage());
+            }
+        }
+    }
 }
--- src/main/java/com/alexandre/server/ChatServer.java
+++ src/main/java/com/alexandre/server/ChatServer.java
@@ -5,15 +5,16 @@ import java.net.ServerSocket;
 public class ChatServer {
 
     private final static int port = 8818;
+    public static ServerSocket serverSocket;
     
-        public static void main(String[] args) {
-            try (ServerSocket serverSocket = new ServerSocket(port)) {
-                System.out.println("Servidor iniciado na porta: " + port);
-                
-            } catch (Exception e) {
-                System.out.println("Erro ao iniciar o servidor 001: " + e.getMessage());
-                System.exit(1);
-                
-            }
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Servidor iniciado na porta: " + port);
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao iniciar o servidor 001: " + e.getMessage());
+            System.exit(1);
+            
+        }
     }
 }
