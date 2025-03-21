763ed8e693c93ef5e9b0d3a32d40ddbf1d57b003
Alexandre <alex12.fernando12@gmail.com>
2025-03-21 02:31:01 -0300

    alexandre

--- src/main/java/com/alexandre/server/ChatServer.java
+++ src/main/java/com/alexandre/server/ChatServer.java
@@ -1,5 +1,19 @@
 package com.alexandre.server;
 
+import java.net.ServerSocket;
+
 public class ChatServer {
 
+    private final static int port = 8818;
+    
+        public static void main(String[] args) {
+            try (ServerSocket serverSocket = new ServerSocket(port)) {
+                System.out.println("Servidor iniciado na porta: " + port);
+                
+            } catch (Exception e) {
+                System.out.println("Erro ao iniciar o servidor 001: " + e.getMessage());
+                System.exit(1);
+                
+            }
+    }
 }
